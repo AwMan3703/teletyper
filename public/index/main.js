661f1e49f93d7fad04b795c9ee037bb3249c7b0c
Aw Man <88689516+AwMan3703@users.noreply.github.com>
2024-09-25 11:29:06 +0200

    Add open chats list to index page

--- public/index/main.js
+++ public/index/main.js
@@ -1,20 +1,34 @@
 "use strict";
 const liveChatsList = document.getElementById("live-chats-list");
 const liveChatElementTemplate = document.getElementById("live-chats-list-item-template");
-fetch('/live-rooms')
-    .then(response => {
-    // Check if the response is OK (status code in the range 200-299)
-    if (!response.ok) {
-        throw new Error('Network response was not ok');
-    }
-    return response.json(); // Parse JSON data
-})
-    .then(data => {
-    console.log('Avaliable rooms:');
-    data.forEach((room) => {
-        console.log('- ' + room.name);
+function _new_liveChatElement(room) {
+    // @ts-ignore
+    const node = liveChatElementTemplate.content.cloneNode(true);
+    const title = node.querySelector(".chat-title");
+    const participants_counter = node.querySelector(".chat-participants-counter");
+    const creation_date = node.querySelector(".chat-creation-time");
+    title.innerText = `${room.owner.username}'s ${room.name}`;
+    participants_counter.innerText = room.participants.length;
+    creation_date.innerText = new Date(room.creation).toLocaleTimeString();
+    return node;
+}
+const updateLiveChatList = () => {
+    fetch('/live-rooms')
+        .then(response => {
+        // Check if the response is OK (status code in the range 200-299)
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json(); // Parse JSON data
+    })
+        .then(data => {
+        data.forEach((room) => {
+            // @ts-ignore
+            liveChatsList.append(_new_liveChatElement(room));
+        });
+    })
+        .catch(error => {
+        console.error('Error updating live chats list:', error);
     });
-})
-    .catch(error => {
-    console.error('Error fetching live chats:', error);
-});
+};
+updateLiveChatList();
--- public/index/main.ts
+++ public/index/main.ts
@@ -3,21 +3,39 @@ const liveChatsList = document.getElementById("live-chats-list");
 const liveChatElementTemplate = document.getElementById("live-chats-list-item-template");
 
 
+function _new_liveChatElement(room) {
+    // @ts-ignore
+    const node = liveChatElementTemplate.content.cloneNode(true)
+    const title = node.querySelector(".chat-title")
+    const participants_counter = node.querySelector(".chat-participants-counter")
+    const creation_date = node.querySelector(".chat-creation-time")
 
-fetch('/live-rooms')
-    .then(response => {
-        // Check if the response is OK (status code in the range 200-299)
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        return response.json(); // Parse JSON data
-    })
-    .then(data => {
-        console.log('Avaliable rooms:')
-        data.forEach((room: { name: string; }) => {
-            console.log('- '+room.name);
+    title.innerText = `${room.owner.username}'s ${room.name}`
+    participants_counter.innerText = room.participants.length
+    creation_date.innerText = new Date(room.creation).toLocaleTimeString()
+
+    return node
+}
+
+const updateLiveChatList = () => {
+    fetch('/live-rooms')
+        .then(response => {
+            // Check if the response is OK (status code in the range 200-299)
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json(); // Parse JSON data
         })
-    })
-    .catch(error => {
-        console.error('Error fetching live chats:', error);
-    });
+        .then(data => {
+            data.forEach((room) => {
+                // @ts-ignore
+                liveChatsList.append(_new_liveChatElement(room))
+            })
+        })
+        .catch(error => {
+            console.error('Error updating live chats list:', error);
+        });
+}
+
+
+updateLiveChatList();
