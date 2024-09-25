2635117e77ed4f80df16812e290ce60b7b17dc4a
Aw Man <88689516+AwMan3703@users.noreply.github.com>
2024-09-25 11:45:42 +0200

    Added dummy rooms

--- roomManager.js
+++ roomManager.js
@@ -4,6 +4,11 @@ exports.liveRooms = void 0;
 const classes_1 = require("./classes");
 // Keep track of currently open rooms
 exports.liveRooms = [
-    new classes_1.Room('test-room', new classes_1.User('test-user')),
-    new classes_1.Room('test-room-2', new classes_1.User('test-user-2'))
+    new classes_1.Room('test-room-1', new classes_1.User('test-user-1')),
+    new classes_1.Room('test-room-2', new classes_1.User('test-user-2')),
+    new classes_1.Room('test-room-3', new classes_1.User('test-user-3')),
+    new classes_1.Room('test-room-4', new classes_1.User('test-user-4')),
+    new classes_1.Room('test-room-5', new classes_1.User('test-user-5')),
+    new classes_1.Room('test-room-6', new classes_1.User('test-user-6')),
+    new classes_1.Room('test-room-7', new classes_1.User('test-user-7'))
 ];
--- roomManager.ts
+++ roomManager.ts
@@ -3,6 +3,11 @@ import {Room, User} from "./classes";
 
 // Keep track of currently open rooms
 export const liveRooms: Room[] = [
-    new Room('test-room', new User('test-user')),
-    new Room('test-room-2', new User('test-user-2'))
+    new Room('test-room-1', new User('test-user-1')),
+    new Room('test-room-2', new User('test-user-2')),
+    new Room('test-room-3', new User('test-user-3')),
+    new Room('test-room-4', new User('test-user-4')),
+    new Room('test-room-5', new User('test-user-5')),
+    new Room('test-room-6', new User('test-user-6')),
+    new Room('test-room-7', new User('test-user-7'))
 ]
