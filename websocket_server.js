28168247a947c8adb44206d42a31972cf5de1069
Aw Man <88689516+AwMan3703@users.noreply.github.com>
2024-09-27 18:35:01 +0200

    started work on websockets

---
+++ websocket_server.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = open_websocket_server;
+function open_websocket_server() {
+}
---
+++ websocket_server.ts
@@ -0,0 +1,6 @@
+
+
+
+export default function open_websocket_server() {
+
+}
