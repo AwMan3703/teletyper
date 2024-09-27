0eac2a7d05fd5b76d3666ff945f469c5fac220e8
Aw Man <brambilladaniele@icloud.com>
2024-09-27 21:09:27 +0200

    working on websockets

--- websocket_server.js
+++ websocket_server.js
@@ -1,5 +1,32 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = open_websocket_server;
-function open_websocket_server() {
+const ws_1 = __importDefault(require("ws"));
+function open_websocket_server(websocket_port) {
+    const server = new ws_1.default.Server({ port: websocket_port });
+    server.on('connection', (client_socket) => {
+        console.log('New websocket connection');
+        // Handle socket errors
+        client_socket.on('error', (error) => {
+            console.error(error);
+        });
+        // Handle message data
+        client_socket.on('message', (data) => {
+            try {
+                const message = JSON.parse(data.toString());
+                switch (message.type) {
+                    case 'join_chat':
+                    // Setup, assign username and room to client, broadcast join event to peers
+                    case 'typing_update':
+                    // Send typing updates to peers
+                }
+            }
+            catch (error) {
+                console.log(`Error parsing websocket message: ${error}`);
+            }
+        });
+    });
 }
--- websocket_server.ts
+++ websocket_server.ts
@@ -1,6 +1,32 @@
+import WebSocket from "ws";
 
 
+export default function open_websocket_server(websocket_port: number) {
+    const server = new WebSocket.Server({port: websocket_port});
 
-export default function open_websocket_server() {
+    server.on('connection', (client_socket: WebSocket) => {
+        console.log('New websocket connection')
 
+        // Handle socket errors
+        client_socket.on('error', (error: Error) => {
+            console.error(error);
+        })
+
+        // Handle message data
+        client_socket.on('message', (data) => {
+            try {
+                const message = JSON.parse(data.toString())
+
+                switch (message.type) {
+                    case 'join_chat':
+                        // Setup, assign username and room to client, broadcast join event to peers
+                    case 'typing_update':
+                        // Send typing updates to peers
+                }
+            }
+            catch (error) {
+                console.log(`Error parsing websocket message: ${error}`);
+            }
+        })
+    })
 }
