0be9e04de6978e6e608485a58c62e78bf8bdb060
Aw Man <88689516+AwMan3703@users.noreply.github.com>
2024-10-13 23:41:32 +0200

    restructured data.ts

--- data.js
+++ data.js
@@ -1,17 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.liveUsers = exports.liveRooms = void 0;
+exports.liveRooms = exports.liveUsers = void 0;
 const classes_1 = require("./classes");
+const utility_1 = require("./utility");
 const server_user = new classes_1.User("server");
-// Keep track of currently open rooms
-exports.liveRooms = [
-    new classes_1.Room('test-room-1', server_user),
-    new classes_1.Room('test-room-2', server_user),
-    new classes_1.Room('test-room-3', server_user, undefined, true, 'pass123'),
-    new classes_1.Room('test-room-4', server_user, 10, true, 'ciaoABC'),
-];
 // Keep track of connected users
 exports.liveUsers = [];
 exports.liveUsers.push(server_user);
+// Keep track of currently open rooms
+exports.liveRooms = [];
+(0, utility_1.createRoom)('test-room-1', server_user);
+(0, utility_1.createRoom)('test-room-2', server_user);
+(0, utility_1.createRoom)('test-room-3', server_user, undefined, true, 'pass123');
+(0, utility_1.createRoom)('test-room-4', server_user, undefined, true, 'ciaoABC');
 const tpr = exports.liveRooms[2]; // Expose a private room for testing
 console.log("Testing private room:", tpr.id, tpr.password);
--- data.ts
+++ data.ts
@@ -1,19 +1,20 @@
 import {Room, User} from "./classes";
+import {createRoom} from "./utility";
 
 const server_user = new User("server")
 
 
-// Keep track of currently open rooms
-export const liveRooms: Room[] = [
-    new Room('test-room-1', server_user),
-    new Room('test-room-2', server_user),
-    new Room('test-room-3', server_user, undefined, true, 'pass123'),
-    new Room('test-room-4', server_user, 10, true, 'ciaoABC'),
-]
-
 // Keep track of connected users
 export const liveUsers: User[] = []
 liveUsers.push(server_user)
+
+// Keep track of currently open rooms
+export const liveRooms: Room[] = []
+createRoom('test-room-1', server_user)
+createRoom('test-room-2', server_user)
+createRoom('test-room-3', server_user, undefined, true, 'pass123')
+createRoom('test-room-4', server_user, undefined, true, 'ciaoABC')
+
 
 const tpr = liveRooms[2] // Expose a private room for testing
 console.log("Testing private room:", tpr.id, tpr.password)
