9d79cf65488e239fb2a17fb3a161e0cb0d7b0588
Aw Man <88689516+AwMan3703@users.noreply.github.com>
2024-10-13 23:21:31 +0200

    removed a few test rooms

--- data.ts
+++ data.ts
@@ -8,12 +8,7 @@ export const liveRooms: Room[] = [
     new Room('test-room-1', server_user),
     new Room('test-room-2', server_user),
     new Room('test-room-3', server_user, undefined, true, 'pass123'),
-    new Room('test-room-4', server_user),
-    new Room('test-room-5', server_user),
-    new Room('test-room-6', server_user, undefined, true, 'ciaoABC'),
-    new Room('test-room-7', server_user),
-    new Room('test-room-8', server_user),
-    new Room('test-room-9', server_user, undefined, true, '0000')
+    new Room('test-room-4', server_user, undefined, true, 'ciaoABC'),
 ]
 
 // Keep track of connected users
