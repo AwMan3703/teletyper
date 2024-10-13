7a5885c40865674bfa09abb8ab3df8a228c2f435
Aw Man <88689516+AwMan3703@users.noreply.github.com>
2024-10-13 14:46:37 +0200

    added server user

--- data.js
+++ data.js
@@ -2,17 +2,18 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.liveUsers = exports.liveRooms = void 0;
 const classes_1 = require("./classes");
+const server_user = new classes_1.User("server");
 // Keep track of currently open rooms
 exports.liveRooms = [
-    new classes_1.Room('test-room-1', new classes_1.User('test-user-1')),
-    new classes_1.Room('test-room-2', new classes_1.User('test-user-2')),
-    new classes_1.Room('test-room-3', new classes_1.User('test-user-3'), undefined, true, 'pass123'),
-    new classes_1.Room('test-room-4', new classes_1.User('test-user-4')),
-    new classes_1.Room('test-room-5', new classes_1.User('test-user-5')),
-    new classes_1.Room('test-room-6', new classes_1.User('test-user-6'), undefined, true, 'ciaoABC'),
-    new classes_1.Room('test-room-7', new classes_1.User('test-user-7')),
-    new classes_1.Room('test-room-8', new classes_1.User('test-user-8')),
-    new classes_1.Room('test-room-9', new classes_1.User('test-user-9'), undefined, true, '0000')
+    new classes_1.Room('test-room-1', server_user),
+    new classes_1.Room('test-room-2', server_user),
+    new classes_1.Room('test-room-3', server_user, undefined, true, 'pass123'),
+    new classes_1.Room('test-room-4', server_user),
+    new classes_1.Room('test-room-5', server_user),
+    new classes_1.Room('test-room-6', server_user, undefined, true, 'ciaoABC'),
+    new classes_1.Room('test-room-7', server_user),
+    new classes_1.Room('test-room-8', server_user),
+    new classes_1.Room('test-room-9', server_user, undefined, true, '0000')
 ];
 // Keep track of connected users
 exports.liveUsers = [];
--- data.ts
+++ data.ts
@@ -1,17 +1,19 @@
 import {Room, User} from "./classes";
 
+const server_user = new User("server")
+
 
 // Keep track of currently open rooms
 export const liveRooms: Room[] = [
-    new Room('test-room-1', new User('test-user-1')),
-    new Room('test-room-2', new User('test-user-2')),
-    new Room('test-room-3', new User('test-user-3'), undefined, true, 'pass123'),
-    new Room('test-room-4', new User('test-user-4')),
-    new Room('test-room-5', new User('test-user-5')),
-    new Room('test-room-6', new User('test-user-6'), undefined, true, 'ciaoABC'),
-    new Room('test-room-7', new User('test-user-7')),
-    new Room('test-room-8', new User('test-user-8')),
-    new Room('test-room-9', new User('test-user-9'), undefined, true, '0000')
+    new Room('test-room-1', server_user),
+    new Room('test-room-2', server_user),
+    new Room('test-room-3', server_user, undefined, true, 'pass123'),
+    new Room('test-room-4', server_user),
+    new Room('test-room-5', server_user),
+    new Room('test-room-6', server_user, undefined, true, 'ciaoABC'),
+    new Room('test-room-7', server_user),
+    new Room('test-room-8', server_user),
+    new Room('test-room-9', server_user, undefined, true, '0000')
 ]
 
 // Keep track of connected users
