befcd20f6bf53a3a1102ac38b77f51ba687c5d14
Aw Man <88689516+AwMan3703@users.noreply.github.com>
2024-10-14 08:31:25 +0200

    implemented definitive data state

--- data.js
+++ data.js
@@ -1,17 +1,20 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.liveRooms = exports.liveUsers = void 0;
-const classes_1 = require("./classes");
 const utility_1 = require("./utility");
-const server_user = new classes_1.User("server");
 // Keep track of connected users
 exports.liveUsers = [];
-exports.liveUsers.push(server_user);
+// Create server user
+const server_user = (0, utility_1.createUser)("server");
 // Keep track of currently open rooms
 exports.liveRooms = [];
-(0, utility_1.createRoom)('test-room-1', server_user);
-(0, utility_1.createRoom)('test-room-2', server_user);
-(0, utility_1.createRoom)('test-room-3', server_user, undefined, true, 'pass123');
-(0, utility_1.createRoom)('test-room-4', server_user, undefined, true, 'ciaoABC');
-const tpr = exports.liveRooms[2]; // Expose a private room for testing
-console.log("Testing private room:", tpr.id, tpr.password);
+(0, utility_1.createRoom)('Public-1', server_user, 10);
+(0, utility_1.createRoom)('Public-2', server_user, 10);
+(0, utility_1.createRoom)('Public-3', server_user, 10);
+exports.liveRooms.forEach(room => {
+    room.message(server_user, {
+        token: server_user.sessionToken,
+        type: "room_message",
+        body: { text: `Welcome to ${room.name}! This is a public room, please be nice :)` }
+    });
+});
--- data.ts
+++ data.ts
@@ -1,20 +1,22 @@
 import {Room, User} from "./classes";
-import {createRoom} from "./utility";
-
-const server_user = new User("server")
+import {createRoom, createUser} from "./utility";
 
 
 // Keep track of connected users
 export const liveUsers: User[] = []
-liveUsers.push(server_user)
+
+// Create server user
+const server_user = createUser("server")
 
 // Keep track of currently open rooms
 export const liveRooms: Room[] = []
-createRoom('test-room-1', server_user)
-createRoom('test-room-2', server_user)
-createRoom('test-room-3', server_user, undefined, true, 'pass123')
-createRoom('test-room-4', server_user, undefined, true, 'ciaoABC')
-
-
-const tpr = liveRooms[2] // Expose a private room for testing
-console.log("Testing private room:", tpr.id, tpr.password)
+createRoom('Public-1', server_user, 10)
+createRoom('Public-2', server_user, 10)
+createRoom('Public-3', server_user, 10)
+liveRooms.forEach(room => {
+    room.message(server_user, {
+        token: server_user.sessionToken,
+        type: "room_message",
+        body: {text: `Welcome to ${room.name}! This is a public room, please be nice :)`}
+    })
+})
